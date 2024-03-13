dd0f068aedbf4e6e71eb423c0fe9c586ef011480
5385825+bobdel@users.noreply.github.com <5385825+bobdel@users.noreply.github.com>
2024-03-12 22:59:33 -0700

    Video 5 completed. Added a custom rating view.

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-    
+
     @Environment(\.modelContext) var modelContext
     @Query var books: [Book]
-    
+
     @State private var showingAddScreen = false
-    
+
     var body: some View {
         NavigationStack {
             Text("Count: \(books.count)")
