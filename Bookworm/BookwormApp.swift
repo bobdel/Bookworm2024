820c6feaa6aa568669b9dbef4c48f7e98d82d4d3
5385825+bobdel@users.noreply.github.com <5385825+bobdel@users.noreply.github.com>
2024-03-12 21:37:50 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Robert DeLaurentis on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
