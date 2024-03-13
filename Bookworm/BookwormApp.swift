b9e696b1be1f0c07a71c9ec4b3a4d10ed839a24d
5385825+bobdel@users.noreply.github.com <5385825+bobdel@users.noreply.github.com>
2024-03-12 22:34:01 -0700

    Video 4 complete. SwiftData model, AddBookView.

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,6 +5,7 @@
 //  Created by Robert DeLaurentis on 3/12/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
